213abd635e09a6928895d1c68d139c98cfe0ce24
Igor <igor777.fesh@mail.ru>
2023-03-07 20:13:44 +0700

    First commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Игорь Фещенко on 06.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
