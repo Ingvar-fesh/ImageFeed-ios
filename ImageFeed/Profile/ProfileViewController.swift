5616735163b13d5f72476cef57c6ba6b74c25904
Igor <igor777.fesh@mail.ru>
2023-03-10 16:01:59 +0700

    Added connections between Profile scene and ProfileViewController

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,10 @@
+import Foundation
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+}
